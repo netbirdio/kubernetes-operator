5b45829a2a9bf60027beb524fea1df1b8b26f9d3
Philip Laine <philip.laine@gmail.com>
2026-05-05 12:14:41 +0200

    Fix SPDX license header
    Signed-off-by: Philip Laine <philip.laine@gmail.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 // Package v1alpha1 contains API Schema definitions for the  v1alpha1 API group.
 // +kubebuilder:object:generate=true
 // +groupName=netbird.io
--- internal/util/ptr.go
+++ internal/util/ptr.go
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
 package util
 
 // Ptr return pointer to any value for API purposes

5e5e37a1538645b0efdc89f9969b313cf4cfb872
Philip Laine <philip.laine@gmail.com>
2026-06-15 13:57:14 +0200

    Add tests to k8sutil (#296)
    Adds more unit tests to sections that are easy to test.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Tests**
    * Added unit tests for Kubernetes owner-reference and finalizer helper
    functions to ensure correct behavior.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- internal/k8sutil/owner.go
+++ internal/k8sutil/owner.go
@@ -3,13 +3,13 @@
 package k8sutil
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
+	kruntime "k8s.io/apimachinery/pkg/runtime"
 	metav1ac "k8s.io/client-go/applyconfigurations/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
 
-func ControllerReference(owner client.Object, scheme *runtime.Scheme) (*metav1ac.OwnerReferenceApplyConfiguration, error) {
+func ControllerReference(owner client.Object, scheme *kruntime.Scheme) (*metav1ac.OwnerReferenceApplyConfiguration, error) {
 	gvk, err := apiutil.GVKForObject(owner, scheme)
 	if err != nil {
 		return nil, err
@@ -23,7 +23,7 @@ func ControllerReference(owner client.Object, scheme *runtime.Scheme) (*metav1ac
 		WithBlockOwnerDeletion(true), nil
 }
 
-func OwnerReference(owner client.Object, scheme *runtime.Scheme) (*metav1ac.OwnerReferenceApplyConfiguration, error) {
+func OwnerReference(owner client.Object, scheme *kruntime.Scheme) (*metav1ac.OwnerReferenceApplyConfiguration, error) {
 	gvk, err := apiutil.GVKForObject(owner, scheme)
 	if err != nil {
 		return nil, err

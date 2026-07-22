fc845f9aa0901a48aeffbf430e5dc2c7090088a5
Philip Laine <philip.laine@gmail.com>
2026-07-22 14:34:59 +0200

    Update kube forwarder version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -9,7 +9,7 @@ import (
 const (
 	NetbirdClientImage       = "ghcr.io/netbirdio/netbird:0.74.7@sha256:b63f4c1584118aeebacfdfd841f0351122a53fccac182b4c43be428c2c9a6b73"
 	KubeApiProxyImage        = "ghcr.io/netbirdio/netbird-kubeapi-proxy:v0.0.4@sha256:bffa4f093abc19b4934ae37657bac76fa3b390cbd39aadac987634215eb750f5"
-	KubeEgressForwarderImage = "ghcr.io/netbirdio/kube-egress-forwarder:v0.0.2@sha256:f3b4637122cbda3c1915d49e6f96edff7e3a3accfceb87b821c829056abe8a6f"
+	KubeEgressForwarderImage = "ghcr.io/netbirdio/kube-egress-forwarder:v0.0.3@sha256:8095e8fe03f28c91b9b7bd55617c582143d66fcc5d9bd8264302c97d03c01360"
 )
 
 func BuildVersion() string {
